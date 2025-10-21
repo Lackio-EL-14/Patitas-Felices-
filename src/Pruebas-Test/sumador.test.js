bf6695c84b561a238181693d21f9327c1ff59d7a
ela251643010 <daniela.condori.b@ucb.edu.bo>
2025-10-21 10:54:58 -0400

    fix: agrego una prueba

--- src/Pruebas-Test/sumador.test.js
+++ src/Pruebas-Test/sumador.test.js
@@ -1,9 +1,9 @@
-// import sumar from "./sumador.js";
+  import sumar from "../Ejemplos base/sumador";
 
-// describe("Sumar", () => {
-//   it("deberia sumar dos numeros", () => {
-//     expect(sumar(3, 2)).toEqual(5);
-//   });
-// });
+ describe("Sumar", () => {
+   it("deberia sumar dos numeros", () => {
+     expect(sumar(3, 2)).toEqual(5);
+   });
+ });
 
 
