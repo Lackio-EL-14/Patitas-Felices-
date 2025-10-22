81a5eb097067cd3c8a276f306a466084d191980c
Camila <camila.carvajal@ucb.edu.bo>
2025-10-22 17:51:44 -0400

    feat: cargado desde el json ver toda la información de un animal rescatado

--- cypress/e2e/lost-dogs.cy.js
+++ cypress/e2e/lost-dogs.cy.js
@@ -1,8 +1,15 @@
 describe("Ver sección de perritos extraviados", () => {
-  it("Deberia visitar página de perritos extraviados", () => {
-    cy.visit("lost-dogs.html"); //Inicializacion
-    // cy.get("#nombre").type("Juan"); //Inicializacion
-    // cy.get("#saludar-button").click(); //Verificacion
-    // cy.get("#resultado-div").should("contain", "Hola Juan"); //Verificacion
-  });
+    it("Deberia visitar página de perritos extraviados", () => {
+        cy.visit("lost-dogs.html"); //Inicializacion
+        // cy.get("#nombre").type("Juan"); //Inicializacion
+        // cy.get("#saludar-button").click(); //Verificacion
+        // cy.get("#resultado-div").should("contain", "Hola Juan"); //Verificacion
+    });
+    it("Deberia Verse toda la información de un animal rescatado", () => {
+        cy.visit("lost-dogs.html");
+        cy.get("#info-animal h2", { timeout: 10000 }).should("contain.text", "Luna");
+        cy.get('h2[name="nombre"]').should("contain.text", "Luna");
+        cy.get('p[name="departamento"]').should("contain.text", "La Paz");
+        cy.get('p[name="raza"]').should("contain.text", "Labrador Retriever");
+    });
 });
