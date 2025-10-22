f66307dba7c30571b6eba05d10a77c902fd91ae8
Lackio-EL-14 <edsonlamas14@gmail.com>
2025-10-22 18:21:06 -0400

    Falla: El panel no muestra la lista de solicitudes, por lo que se empezara un ciclo TDD para implementar reglas de negocio que solventen esto

--- cypress/e2e/panel-solicitudes-adopcion.cy.js
+++ cypress/e2e/panel-solicitudes-adopcion.cy.js
@@ -3,7 +3,17 @@ describe ('Panel de Solicitudes', () => {
         cy.visit('/');
         cy.get('[data-cy="link-panel-solicitudes"]').should('have.attr', 'href', '/panel-solicitudes-adopcion.html');
         cy.get('[data-cy="link-panel-solicitudes"]').click();
-        cy.url().should('include', '/panel-solicitudes-adopcion.html')
-       
+        cy.url().should('include', '/panel-solicitudes-adopcion.html')  
+    });
+    it ('Deberiamos ver las solicitudes de adopcion en el panel', () => {
+        const solicitudesMock = [
+        { id: 1, nombre: 'Juan Perez', mascota: 'Fido', estado: 'Pendiente' }//,
+        //{ id: 2, nombre: 'Ana Gomez', mascota: 'Misu', estado: 'Pendiente' }
+        ];
+        cy.intercept('GET', '/api/solicitudes', { body: solicitudesMock }).as('getSolicitudes');
+        cy.visit('/panel-solicitudes-adopcion.html');
+        cy.wait('@getSolicitudes');
+        cy.contains('Juan Perez').should('be.visible');
     });
 });
+
