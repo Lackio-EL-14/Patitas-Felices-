4e9514e59300b9d3914441d4352f6d9c02f40ea6
Camila <camila.carvajal@ucb.edu.bo>
2025-10-22 17:05:04 -0400

    fix: test Linkear desde inicio a una nueva página y Mostrar el título 'Perritos extraviados'

--- cypress/e2e/lost-dogs.cy.js
+++ cypress/e2e/lost-dogs.cy.js
@@ -1,3 +1,8 @@
-// describe(){
-
-// }
+describe("Ver sección de perritos extraviados", () => {
+  it("Deberia visitar página de perritos extraviados", () => {
+    cy.visit("lost-dogs.html"); //Inicializacion
+    // cy.get("#nombre").type("Juan"); //Inicializacion
+    // cy.get("#saludar-button").click(); //Verificacion
+    // cy.get("#resultado-div").should("contain", "Hola Juan"); //Verificacion
+  });
+});
